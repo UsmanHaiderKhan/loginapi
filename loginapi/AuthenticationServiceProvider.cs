97a7edff16ee6a70bda4e28b4f0df81ad47e755a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-06-22 03:07:20 -0700

    Add project files.

---
+++ loginapi/AuthenticationServiceProvider.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace loginapi
+{
+    public class AuthenticationServiceProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            if (context.UserName == "Admin" && context.Password == "Admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("UserName", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Usman haider khan"));
+                context.Validated(identity);
+
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("UserName", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Arslan Ameer"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("Permission DENIED", "Invalid UserName Or Password... !Try Again");
+                return;
+            }
+        }
+    }
+}
---
+++ loginapi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace loginapi.Controllers
+{
+    public class UserController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/user/getall")]
+        public IHttpActionResult Get()
+        {
+            return Ok(DateTime.Now.ToString());
+
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/user/userAuth")]
+        public IHttpActionResult GetAuth()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok("Hello" + identity.Name);
+        }
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("api/user/authorize")]
+        public IHttpActionResult GetAuthForAdmin()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var roles = identity.Claims.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value);
+
+            return Ok("Hello" + identity.Name + "Role" + string.Join(",", roles.ToList()));
+        }
+    }
+}
